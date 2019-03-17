d3829b6a2fc5f96544524da933f8e050b08f7ba0
Christian Wansart <christian.wansart@openknowledge.de>
2019-03-17 14:48:55 +0100

    fix api urls

--- src/app/todos/service/config.service.ts
+++ src/app/todos/service/config.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ConfigService {
-  public readonly restBackendUrl = 'http://localhost:8080/api';
-  public readonly graphqlBackendUrl = 'http://localhost:8080/graphql';
+  public readonly restBackendUrl = 'http://localhost:8080/todo-list-rest/api';
+  public readonly graphqlBackendUrl = 'http://localhost:8081/todo-list-graphql/';
 }
